9033347bb426c9c510dd077f5382c93f26825921
dongjuanyong <38952488+dongjuanyong@users.noreply.github.com>
2020-06-25 23:13:25 +0800

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -5,6 +5,7 @@ cp ../../../intel_aes_lib/coremark utils/coremark/coremark
 cp ../../../intel_aes_lib/coremark.sh utils/coremark/coremark.sh
 mkdir -p net/dnscrypt-proxy2/patches
 cp ../../../intel_aes_lib/999-add-tcp-fastopen-support.patch net/dnscrypt-proxy2/patches/999-add-tcp-fastopen-support.patch
+cp ../../../intel_aes_lib/999-add-support-for-libplist-2.2.patch sound/forked-daapd/patches/999-add-support-for-libplist-2.2.patch
 cd ../luci
 patch -p1 < ../../../intel_aes_lib/luci.patch
 cd ../..
