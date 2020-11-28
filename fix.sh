823a5e963a61fde113a12bd39d57e72522907ab5
build <build@Apollo.localdomain>
2020-11-28 11:11:44 +0800

    remove tfo patch

--- fix.sh
+++ fix.sh
@@ -3,8 +3,6 @@ cd openwrt/feeds/packages
 patch -p1 < ../../../intel_aes_lib/packages.patch
 cp ../../../intel_aes_lib/coremark utils/coremark/coremark
 cp ../../../intel_aes_lib/coremark.sh utils/coremark/coremark.sh
-mkdir -p net/dnscrypt-proxy2/patches
-cp ../../../intel_aes_lib/999-add-tcp-fastopen-support.patch net/dnscrypt-proxy2/patches/999-add-tcp-fastopen-support.patch
 cd ../luci
 patch -p1 < ../../../intel_aes_lib/luci.patch
 cd ../..
