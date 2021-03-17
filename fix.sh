09ce6879aeb388db31142de2db624d221215c0dd
build <build@Apollo.localdomain>
2021-03-17 21:42:17 +0800

    compiling fix

--- fix.sh
+++ fix.sh
@@ -2,6 +2,7 @@
 cd openwrt/feeds/packages
 rm -rf net/xray-core sound/shairport-sync/patches/010-no-cxx.patch
 ln -s ../../helloworld/xray-core net/xray-core
+sed -i '/bin\/v2ray/d' net/xray-core/Makefile
 patch -p1 < ../../../intel_aes_lib/packages.patch
 cp ../../../intel_aes_lib/coremark utils/coremark/coremark
 cp ../../../intel_aes_lib/coremark.sh utils/coremark/coremark.sh
