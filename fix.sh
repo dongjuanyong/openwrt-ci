6ab5ec7357100a9a5a4f45fc1641a4f813d4f936
build <build@Apollo.localdomain>
2021-08-30 09:45:44 +0800

    refresh patches

--- fix.sh
+++ fix.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd openwrt/feeds/packages
-ln -s ../../helloworld/xray-core net/xray-core
-sed -i '/bin\/v2ray/d' net/xray-core/Makefile
+cp ../helloworld/xray-core/Makefile net/xray-core/Makefile
 patch -p1 < ../../../patches/packages.patch
 cp ../../../patches/coremark utils/coremark/coremark
 cp ../../../patches/coremark.sh utils/coremark/coremark.sh
